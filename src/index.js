87f0c5a26f0eb1ab1469a508b32e3744730b7b19
Zyro <ghifarim68@gmail.com>
2021-11-02 11:22:49 +0700

    Combine all of the feature

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './Home';
+import App from './Home.js';
+import Contact from './Contact';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
