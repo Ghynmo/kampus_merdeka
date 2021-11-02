16253fa213c4e7881e17bab124f1efbddd0251c7
Zyro <ghifarim68@gmail.com>
2021-11-02 18:59:39 +0700

    Update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './Home';
-import Contact from './Contact';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
