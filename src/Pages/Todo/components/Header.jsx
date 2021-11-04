c0f088aa7c789af2cb439935b198bb0cf6e300b4
M Ghifari <ghifarim68@gmail.com>
2021-11-04 12:17:27 +0700

    Trying EventHandle

--- src/Pages/Todo/components/Header.jsx
+++ src/Pages/Todo/components/Header.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 import styles from './Header.module.css'
 
 export default function Header() {
+
+    function eventClick() {
+        const theTitle = document.querySelector('div h1');
+        theTitle.style.color = 'red';
+    }
+
+
     return (
-        <div className={styles.navigation}>
+        <div className={styles.navigation} onClick={() => eventClick()}>
             <h1>To Do App</h1>
         </div>
     )
