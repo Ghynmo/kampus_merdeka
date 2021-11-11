f282e99830de5027271ed87cc06f75640436b299
M Ghifari <ghifarim68@gmail.com>
2021-11-11 12:24:01 +0700

    Using redux

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import "./App.css"
 import { BrowserRouter as Router } from "react-router-dom"
+import { Provider } from "react-redux"
+import { PersistGate } from "redux-persist/integration/react"
 
 import TodoContainer from "./component/TodoContainer"
+import { store, persistor } from "./store/store"
 
 function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
-      <TodoContainer />
-    </Router>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Router basename={process.env.PUBLIC_URL}>
+          <TodoContainer />
+        </Router>
+      </PersistGate>
+    </Provider>
   )
 }
 
