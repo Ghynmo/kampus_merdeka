9935fe28e81b3d3c7fedc1c1ed594ebc3c3d5433
M Ghifari <ghifarim68@gmail.com>
2021-11-14 14:22:30 +0700

    Update Redux Passenger

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
-// import Home from "./components/Home";
-// import "./App.css"
-// import { Provider } from "react-redux";
-// import { PersistGate } from "redux-persist/integration/react";
-// import { persistor, store } from "./store/store";
+import Home from "./components/Home";
+import "./App.css"
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor, store } from "./store/store";
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <Provider store={store}>
-//         <PersistGate loading={null} persistor={persistor}>
-//           <Home/>
-//         </PersistGate>
-//       </Provider>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-import FormPage from "./Form/FormPage";
-import Search from "./Form/Search";
 function App() {
   return (
-    <>
-      <FormPage />
-      <Search />
-    </>
+    <div className="App">
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <Home/>
+        </PersistGate>
+      </Provider>
+    </div>
   );
 }
 
 export default App;
+
+// import FormPage from "./Form/FormPage";
+// import Search from "./Form/Search";
+// function App() {
+//   return (
+//     <>
+//       <FormPage />
+//       <Search />
+//     </>
+//   );
+// }
+
+// export default App;
--- src/store/passengerSlice.js
+++ src/store/passengerSlice.js
@@ -1,8 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit'
 import { v4 as uuidv4 } from "uuid"
 
-export { createSlice } from '@reduxjs/toolkit'
-
 const initialValue = [
     {
         id : uuidv4(),
